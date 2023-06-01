3603e78f0abc241e8365e609aaf9dd73d961cc14
Philippe Marschall <philippe.marschall@netcetera.com>
2023-06-01 17:37:44 +0200

    API updates

--- src/main/java/com/github/marschall/sqlstringtemplate/RowMapper.java
+++ src/main/java/com/github/marschall/sqlstringtemplate/RowMapper.java
@@ -9,18 +9,23 @@ public interface RowMapper<T> {
 
   T mapRow(ResultSet resultSet) throws SQLException;
 
-  public static RowMapper<String> string() {
+  public static RowMapper<String> ofString() {
     return resultSet -> resultSet.getString(1);
   }
 
-  public static RowMapper<Integer> integer() {
+  public static RowMapper<Integer> ofInteger() {
     return resultSet -> resultSet.getInt(1);
   }
-  public static RowMapper<BigDecimal> bigDecimal() {
+
+  public static RowMapper<Long> ofLong() {
+    return resultSet -> resultSet.getLong(1);
+  }
+
+  public static RowMapper<BigDecimal> ofBigDecimal() {
     return resultSet -> resultSet.getBigDecimal(1);
   }
 
-  public static <T> RowMapper<T> object(Class<T> clazz) {
+  public static <T> RowMapper<T> ofObject(Class<T> clazz) {
     return resultSet -> resultSet.getObject(1, clazz);
   }
 
